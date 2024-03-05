88926da0e8465113fbdce63fbc3bf246477b7186
bobbyhadz <opmftw@gmail.com>
2024-03-05 07:12:09 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const box = document.getElementById('box');
+
+// ✅ Set width to 100px
+box.style.width = '100px';
+
+// ✅ Set height to 100px
+box.style.height = '100px';
